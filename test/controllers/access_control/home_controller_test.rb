b6f5e7b635a86579a405b2f88b289f6b33a354b9
Titi Wangsa bin Damhore <blacksnow666@gmail.com>
2014-09-24 13:42:59 +0800

    better test case, for home controller

--- test/controllers/access_control/home_controller_test.rb
+++ test/controllers/access_control/home_controller_test.rb
@@ -3,11 +3,13 @@ require 'test_helper'
 module AccessControl
   class HomeControllerTest < ActionController::TestCase
 
-     test "the truth" do
-       sut = HomeController.new
-       sut.index
-       assert true
+     test "index is defined" do
+       assert HomeController.new.respond_to?('index')
      end
+
+    test "chair is not defined" do
+      assert_not HomeController.new.respond_to?('chair')
+    end
 
   end
 end
