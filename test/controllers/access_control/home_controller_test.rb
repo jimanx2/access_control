a60e38465e3a52b57988a1cd874246601bba66cd
Titi Wangsa bin Damhore <blacksnow666@gmail.com>
2014-09-24 13:33:56 +0800

    simple test case for simple controller

--- test/controllers/access_control/home_controller_test.rb
+++ test/controllers/access_control/home_controller_test.rb
@@ -2,8 +2,12 @@ require 'test_helper'
 
 module AccessControl
   class HomeControllerTest < ActionController::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+
+     test "the truth" do
+       sut = HomeController.new
+       sut.index
+       assert true
+     end
+
   end
 end
